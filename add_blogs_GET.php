97312fd95eebc20b4dc9a10d96b59edecb8e3b14
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-05 04:12:27 +0530

    Add files via upload

---
+++ add_blogs_GET.php
@@ -0,0 +1,37 @@
+<?php
+require 'connect.php';
+require 'session_script.php';
+
+$blog_pic=$_POST['blog_pic'];
+$blog_category=$_POST['blog_category'];
+$blog_title=$_POST['blog_title'];
+
+$blog_desc=$_POST['blog_desc'];
+$created_by=$_SESSION['user_id'];
+
+
+
+
+$sql="INSERT INTO blogs(blog_pic, blog_category, blog_title, blog_desc, user_id) VALUES (:blog_pic,:blog_category,:blog_title,:blog_desc,:created_by)";
+$stmt= $conn->prepare($sql);
+$result= $stmt->execute(array(
+	':blog_pic'=>$blog_pic,
+	':blog_category'=>$blog_category,
+	':blog_title'=>$blog_title,
+	':blog_desc'=>$blog_desc,
+	':created_by' =>$created_by
+));
+
+
+if($result)
+{
+	echo "1";
+}
+else
+{
+	echo "0";
+}
+
+
+
+?>
