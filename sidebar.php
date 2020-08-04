3bf3dd672e78867b871ecf1053fea7b65c8b5574
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-04 17:42:47 +0530

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,66 @@
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div style="margin-left:-200px" id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <a style="color: white" href="#">
+                    PROFILE
+                </a>
+
+
+
+               
+
+              
+           </li>
+           <li>
+            <a style="color: white" href="#">DASHBOARD</a>
+        </li>
+        <li>
+            <a style="color: white" href="add_blogs.php">ADD BLOGS</a>
+        </li>
+        <li>
+            <a style="color: white" href="#">VIEW BLOGS</a>
+        </li>
+        <li>
+            <a style="color: white" href="#">EVENTS</a>
+        </li>
+        <li>
+            <a style="color: white" href="#">ABOUT</a>
+        </li>
+        <li>
+            <a style="color: white" href="#">SERVICES</a>
+        </li>
+        <li>
+            <a style="color: white" href="#">CONTACT</a>
+        </li>
+    </ul>
+</div>
+<!-- /#sidebar-wrapper -->
+
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1></h1>
+                <p></p>
+                <p></p>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /#page-content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+<!-- Menu Toggle Script -->
+   <!--  <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+ -->
