baec312736df0defa4d4f3c2e0af0c8eb315c8f0
lloyddias <40758816+lloyddias@users.noreply.github.com>
2020-08-04 19:16:01 +0530

    Add files via upload

---
+++ login_GET.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+require 'connect.php';
+
+error_reporting(0);
+
+$uname=$_POST['uname'];
+$pass=($_POST['pass']);
+
+
+
+
+
+
+$sql= $conn->prepare("SELECT * FROM users where username='".$uname."'and password='".$pass."'");
+$sql->execute();
+$sql->setFetchMode(PDO::FETCH_ASSOC);
+if($sql->rowCount()>0){
+	foreach (($sql->fetchAll()) as $key => $row) 
+		
+
+	$_SESSION["email"] = $row['user_email'];
+    $_SESSION["user_name"] = $row['user_name'];
+    
+	$_SESSION["password1"] = $row['password'];
+	
+	$_SESSION["type_id"] = $row['user_type'];
+	$_SESSION["user_id"] = $row['user_id'];
+	$_SESSION["profile_pic"] = $row['profile_pic'];
+
+
+
+
+
+	if ($row['user_type']=="0") {
+
+		echo "20";
+
+	}
+	else if ($row['user_type']=="1") {
+
+		echo "23";
+
+
+	}
+
+	else
+	{
+		echo '0';
+	}
+}
+
+
+
+
+?>
+
---
+++ session_script.php
@@ -0,0 +1,28 @@
+<?php 
+
+session_start();
+require 'connect.php';
+if (isset($_SESSION["username"])){
+
+// echo $_SESSION["first_name"];
+	$sql= $conn->prepare("SELECT * FROM users where username= '".$_SESSION['user_email']."' and password='".$_SESSION['password']."'");
+	$sql->execute();
+	$sql->setFetchMode(PDO::FETCH_ASSOC);
+	if($sql->rowCount()>0){
+
+		$user_name =$_SESSION["user_name"];
+        $user_id =$_SESSION["user_id"];
+		// $user_type =$_SESSION["user_type"] ;
+		// $type_id= $_SESSION["type_id"] ;
+
+
+	}else{
+		
+	}
+
+
+}else{
+	
+}
+
+?>
