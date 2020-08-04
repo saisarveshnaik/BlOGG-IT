9e59ab30c6b3d4267125cc9a4a82f142072f3df2
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-04 19:06:16 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,124 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
 
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 style="color: white" class="font-weight-bold text-uppercase mb-4">Blogging website</h5>
+        <p style="color: white">This website was deveoped as a Web technology project</p>
+        <p></p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 style="color: white" class="font-weight-bold text-uppercase mb-4">About</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a style="color: white" href="#!">PROJECTS</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a style="color: white" href="#!">ABOUT US</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a style="color: white" href="#!">BLOG</a>
+            </p>
+          </li>
+          <li>
+            <p>
+             <!--  <a href="#!">AWARDS</a> -->
+            </p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 style="color: white" class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+        <ul class="list-unstyled">
+          <li>
+
+              <p style="color: white">Curchorem, Goa</p>
+          </li>
+          <li>
+            <p style="color: white">saisarvesh.naik007@gmail.com</p>
+          </li>
+          <li>
+            <p style="color: white">+918007023761</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-print mr-3"></i></p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 style="color: white" class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+        <!-- Facebook -->
+        <a type="button" class="btn-floating btn-fb">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <!-- Twitter -->
+        <a type="button" class="btn-floating btn-tw">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <!-- Google +-->
+        <a type="button" class="btn-floating btn-gplus">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+        <!-- Dribbble -->
+        <a type="button" class="btn-floating btn-dribbble">
+          <i class="fab fa-dribbble"></i>
+        </a>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/">Blogging.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
