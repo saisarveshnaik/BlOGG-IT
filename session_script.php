37a1cc291e0b9ff342b505519be02b638519d079
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-05 04:26:35 +0530

    Add files via upload

--- session_script.php
+++ session_script.php
@@ -5,13 +5,14 @@ require 'connect.php';
 if (isset($_SESSION["username"])){
 
 // echo $_SESSION["first_name"];
-	$sql= $conn->prepare("SELECT * FROM users where username= '".$_SESSION['user_email']."' and password='".$_SESSION['password']."'");
+	$sql= $conn->prepare("SELECT * FROM users where username= '".$_SESSION['username']."' and password='".$_SESSION['password1']."'");
 	$sql->execute();
 	$sql->setFetchMode(PDO::FETCH_ASSOC);
 	if($sql->rowCount()>0){
 
 		$user_name =$_SESSION["user_name"];
         $user_id =$_SESSION["user_id"];
+        
 		// $user_type =$_SESSION["user_type"] ;
 		// $type_id= $_SESSION["type_id"] ;
 
