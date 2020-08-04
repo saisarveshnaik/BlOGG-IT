45149fcbb3b136649def36a1a08566c7bc95c95e
lloyddias <40758816+lloyddias@users.noreply.github.com>
2020-08-04 19:15:00 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,9 +1,19 @@
+<?php
+require "session_script.php";
+?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" style="margin-left: 320px" href="#">BLOGGING</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+
+ 
+
+
+
+
+
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
@@ -33,12 +43,19 @@
     <form class="form-inline my-2 my-lg-0 form-navbar">
       <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
 
-    
-    <button class="btn btn-outline-default"><a href="logout.php" style="color:black"> LOGOUT </a></button>;
+    <?php
+              
+               if (isset($_SESSION['user_name'])) {
+                 echo '<img class="img-circle profile_img" width="200px" src="'.$_SESSION["profile_pic"].'" alt="images/profile.png">';
+echo 'Hi '.$_SESSION["user_name"].' | <button class="btn btn-outline-default"><a href="logout.php" style="color:black">LOGOUT </a></button>';
 
-      <button class="btn btn-outline-default"><a href="login.php" style="color:black">LOGIN </a></button>
-      <button class="btn btn-success margin-left"><a href="register.php" style="color:white">REGISTER</a></button>;
-      
+               }
+               else{
+                echo '<button class="btn btn-outline-default"><a href="login.php" style="color:black">LOGIN </a></button>
+      <button class="btn btn-success margin-left"><a href="register.php" style="color:white">REGISTER</a></button>';
+               }
+               
+              ?>
      
     </form>
   </div>
