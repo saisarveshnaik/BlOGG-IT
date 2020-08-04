75e2deb573957e4b9d5762150d3c04ea537f624e
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-04 23:50:24 +0530

    Update login.php

--- login.php
+++ login.php
@@ -23,7 +23,55 @@
   ?>
 
 
-  <section class="ftco-section contact-section ftco-degree-bg">
+
+  <div class="container" style="margin-top: 100px;">
+    <div class="row">
+      <div class="col-md-3">
+
+      </div>
+      <div class="col-md-6">
+        <h4 class="mb-4" align="center" style="color: white" >LOGIN TO YOUR ACCOUNT</h4>
+        <div class="form-group">
+          <label style="color: white">USERNAME</label>
+          <input type="text" id="uname" class="form-control" placeholder="Your username">
+        </div>
+        <div class="form-group">
+          <label style="color: white">PASSWORD</label>
+          <input type="password" id="pass" class="form-control" placeholder="Your password">
+        </div> 
+      </div>
+      <div class="col-md-3">
+
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div style="text-align: center;" class="col-md-6">
+
+
+
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <input type="button" class="btn btn-primary py-3 px-5" id="login" value="LOGIN" style="margin-right: 10px"> 
+        <button style="margin-left: 10px;" type="button" class="btn btn-primary py-3 px-5"><a href="register.php" style="color: white;">Register</a></button>
+      </div>
+
+        
+        
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+
+
+
+  </div>
+
+
+
+
+
+
+<!--   <section class="ftco-section contact-section ftco-degree-bg">
     <div class="container">
       <div class="row d-flex mb-5 contact-info">
 
@@ -33,34 +81,43 @@
       <form action="#">
         <div class="row block-9">
           <div class="col-md-12">
-            <h4 class="mb-4" align="center" style="color: white" >LOGIN TO YOUR ACCOUNT</h4>
+             
+            <div class="col-md-3">
+              
+            </div>
+            <div class="col-md-6">
+              <h4 class="mb-4" align="center" style="color: white" >LOGIN TO YOUR ACCOUNT</h4>
             
             <div class="form-group">
-              <label style="color: white">USERNAME(EMAIL)</label>
+              <label style="color: white">USERNAME</label>
               <input type="text" id="uname" class="form-control" placeholder="Your username">
             </div>
             <div class="form-group">
               <label style="color: white">PASSWORD</label>
               <input type="password" id="pass" class="form-control" placeholder="Your password">
             </div>
-            
+            </div>
+            <div class="col-md-3"></div>
+ 
+            </div>
 
-          </div>
 
-          <div class="col-md-4"></div>
-          <div class="col-md-2">     
+          <div class="row">
+            <div style="margin-left: 100px" class="col-md-6">     
             <input type="button" class="btn btn-primary py-3 px-5" id="login" value="LOGIN">
           </div>
-          <div class="col-md-2"> 
-            <p><a href="register.php" class="btn btn-primary px-4 py-3">REGISTER NOW</a> </p></div>
-
-
-            
-            
+          <div style="margin-left: 100px" class="col-md-6"> 
+            <p><a href="register.php" class="btn btn-primary px-4 py-3">REGISTER NOW</a> </p>
           </div>
+            </div>
+
+
+          </div>
+
+  
         </form>
       </div>
-    </section>
+    </section> -->
 
 
     <?php
