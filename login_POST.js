543620d63f45eabfbbeafc5052c23fb4aec252cf
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-05 00:09:05 +0530

    Add files via upload

---
+++ login_POST.js
@@ -0,0 +1,61 @@
+$("#login").click(function(){
+
+  var uname=$("#uname").val();
+  var pass=$("#pass").val();
+
+  var datastr='uname='+uname+'&pass='+pass;
+
+
+
+
+
+
+
+  if ($("#uname").val().length==0 || $("#pass").val().length==0) {
+   alert("Please fill username and password");
+ }
+
+ else{
+
+   $.ajax({
+    
+    
+    type: "POST",
+    url: "login_GET.php",
+    data: datastr,
+    cache: false,
+    success: function(res){
+      if(res==20)
+      {
+        // window.location.href ="admin/production/index.php";
+        alert("Admin loign!");
+        window.location.href ="index.php";
+        
+        
+      }
+      else if(res==23){
+        
+       
+        alert("User login!");
+
+       window.location.href ="index.php";
+
+       
+
+
+     }
+                    
+                    else{
+                      alert("invalid login");
+                    }
+                    
+                  }
+                }); 
+ }
+ 
+
+
+
+
+
+});
