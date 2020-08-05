2331cb5815a0e0d8a7d2de905a32d1f7c2b4a861
lloyddias <40758816+lloyddias@users.noreply.github.com>
2020-08-05 13:20:08 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -23,76 +23,7 @@
 	?>
 
 
-
-	<div class="container" style="margin-top: 2%;">
-		<div class="row">
-			<div class="col-md-3"></div>
-
-			<div class="col-md-6">
-
-				<h4 style="color: white;text-align: center;" class="mb-4">REGISTER FOR A NEW ACCOUNT</h4>
-
-				<div class="row">
-					<div class="col-md-4">
-						<div class="form-group">
-							<label style="color: white">Profile Picture</label>	
-							<input type="file" id="profile_pic" name="file" />
-						</div>
-					</div>
-					<div class="col-md-8" style="text-align: center;">
-						<img src="images/profile.png" id="img" width="100" height="100" align="right">
-						<div><input type="hidden" name="image1" id="image1" disabled></div>
-
-
-					</div>
-				</div>
-
-
-				<div class="form-group">
-					<label style="color: white">Full Name</label>
-					<input type="text" class="form-control" id="user_name" placeholder="Your Name">
-				</div>
-				<div class="form-group">
-					<label style="color: white">Address</label>
-					<input type="text" class="form-control" id="user_address" placeholder="Your Address">
-				</div>
-				<div class="form-group">
-					<label style="color: white">Contact No</label>
-					<input type="number" class="form-control" id="user_cont" placeholder="Your Contact Number">
-				</div>
-				<div class="form-group">
-					<label style="color: white">Email</label>
-					<input type="text" class="form-control" id="user_email" placeholder="Your Email">
-				</div>
-				<div class="form-group">
-					<label style="color: white">Username(must be unique)</label>
-					<input type="text" class="form-control" id="username" placeholder="Create Unique Username">
-				</div>
-				<div class="form-group">
-					<label style="color: white">Password</label>
-					<input type="password" class="form-control" id="password" placeholder="Your Password">
-				</div>
-
-				<div class="form-group">
-					<label style="color: white">Gender:</label><br>
-					<input type="radio" id="gender" value="male"> Male<br>
-					<input type="radio" id="gender" value="female"> Female<br>
-				</div>
-
-                <div style="text-align: center;">
-				<button type="button" id="formsubmit" class="btn btn-primary py-3 px-5">REGISTER</button>
-                </div>
-
-
-
-					</div>
-
-			<div class="col-md-3"></div>
-		</div>
-
-		</div>
-
-	<!-- <section class="ftco-section contact-section ftco-degree-bg">
+	<section class="ftco-section contact-section ftco-degree-bg">
 		<div class="container">
 			<div class="row d-flex mb-5 contact-info">
 
@@ -119,6 +50,9 @@
 						</div>
 						<div class="form-group">
 							<input type="text" class="form-control" id="username" placeholder="Create Unique Username">
+							<small id="limit" style="color: red; "></small>
+							<small id="flag"></small>
+							
 						</div>
 						<div class="form-group">
 							<input type="password" class="form-control" id="password" placeholder="Your Password">
@@ -132,7 +66,7 @@
 					<div class="col-md-6 pr-md-5">
 						<br><br>
 
-						
+						<!--------------------------------PROFILE PIC------------------------------------------------------>
 						<div class="item form-group">
 							<label style="color: white" class="col-form-label col-md-3 col-sm-3 label-align" for="middle-name">Profile Picture<span class="required">*</span>
 							</label>
@@ -159,7 +93,7 @@
 						</div>
 
 
-						<button type="button" id="formsubmit" class="btn btn-primary py-3 px-5">REGISTER</button>
+						<button type="button" id="formsubmit" class="btn btn-primary py-3 px-5" style="display:none;">REGISTER</button>
 					</div>
 
 
@@ -168,7 +102,7 @@
 			</div>
 		</form>
 	</div>
-</section> -->
+</section>
 
 
 <?php
@@ -207,4 +141,62 @@ require 'footer.php';
 
 </script>
 </body>
+<script>
+
+
+$( "#username" ).keyup(function() {
+
+
+	$('#username').on('blur', function(){
+    if(this.value.length < 5){ 
+       $('#limit').html('Should Be Atleast 5 Characters!');
+   		}
+
+       var username=$("#username").val();
+
+       var datastr='username='+username;
+
+
+       $.ajax({
+    
+	    
+	    type: "POST",
+	    url: "usearch.php",
+	    data: datastr,
+	    cache: false,
+	    success: function(res){
+
+	      if(res=='1')
+	      {
+	        $('#limit').html('Username Already Taken!');
+	        
+	        
+	      }
+	  else if(res=='0'){
+	       
+	       $('#limit').html('');
+	       $('#formsubmit').css('display','inline');
+	      }
+	                    
+	                  }
+	                }); 
+   }
+	 
+	 );
+
+
+
+
+
+       $(this).focus(); // focuses the current field.
+       return false; // stops the execution.
+    
+});
+
+
+
+   
+  
+</script>
+
 </html>
---
+++ usearch.php
@@ -0,0 +1,33 @@
+<?php 
+
+session_start();
+
+error_reporting(0);
+
+$Username=$_POST['username'];
+
+   	
+    require 'connect.php';
+    $stmt = $conn->prepare('SELECT * FROM users');
+    $stmt->execute(); $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    if($stmt->rowCount()>0){
+      foreach(($stmt->fetchAll()) as $k1=>$row) { 
+      	
+        if( $row['username'] == $Username ){
+          
+        	echo '1';
+        }
+        else
+        {
+        	echo '0';
+        }
+
+      }
+
+    }
+
+    
+
+
+
+                  ?>
