9b74da49b1fed481c470b37160a08950ca1a89e5
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-07 16:01:26 +0530

    Fixed the layout of view blogs and update blogs.

--- view_blogs/update_blog.php
+++ view_blogs/update_blog.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Blogging|Add Blogs</title>
+	<title>Blogging | UPDATE BLOG</title>
 
 
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+
 	<link rel="stylesheet" type="text/css" href="../style.css">
 
 	<link rel="stylesheet" type="text/css" href="../sidebar.css">
-
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 
 
 
 </head>
 <body>
+
 
 	<?php
 	require '../header.php';
@@ -27,44 +27,23 @@
 
 
 
-	<div class="container" style="margin-top: 10px">
-		<form method="post" action="" enctype="multipart/form-data" id="myform">
-		<div class="row">
-			<div class="col-md-6">
+
+	<section>
+		<div class="container">
+			<form method="post" action="" enctype="multipart/form-data">
+
 				<div class="row">
-
-					<div class="col-md-2"></div>
-					<div class="col-md-10">
-						<h4 style="color: white;text-align: center;" class="mb-4">CREATE A NEW BLOG!</h4>	
-
-
-
+					<!-- <div class="col-md-2"></div> -->
+					<div class="col-md-12 text-center">
+						<h1 style="color: white">Update your blog</h1>
+					</div>
+				</div>
 
 
-						<!-- <div class="item form-group">
-							<select name="category" id="blog_category" class="form-control"> 
-								<option value="" > Select Category</option>
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6 text-center" style="margin-top: 50px">
 
-								<?php 
-
-								require "../connect.php"; 
-								$stmt = $conn->prepare("SELECT * FROM categories");
-								$stmt->execute();
-								$stmt->setFetchMode(PDO::FETCH_ASSOC);
-								if($stmt->rowCount()>0){
-									foreach(($stmt->fetchAll()) as $k1=>$row) {
-
-										echo ' <option value="'.$row['category_id'].'"> '.$row['category_name'].' </option>';
-
-
-									}
-								}
-
-								?>
-
-
-							</select>      
-						</div> -->
 
 						<?php
 							require '../connect.php';
@@ -76,130 +55,136 @@
 							            if($sql->rowCount()>0){
 				              foreach (($sql->fetchAll()) as $key => $row) {
 
-				              	echo '	<div class="item form-group">
-				              				<input type=button id="blog_id" style="display:none;" value="'.$blog_id.'">
-											<input type="text"  required="required" class="form-control " id="blog_title" name="productname" value="'.$row['blog_title'].'">
-										</div>
+				              	echo '
 
-										<div class="item form-group">
-											<textarea class="form-control blogTextarea" type="text" name="productnote"  id="blog_desc"> '.$row['blog_desc'].' </textarea>
-										</div>';
-										
-									}
+						<div class="item form-group">
+							<input type=button id="blog_id" style="display:none;" value="'.$blog_id.'">
+							<input type="text"  required="required" class="form-control " id="blog_title" name="productname" value="'.$row['blog_title'].'">
+						</div>
+						<div class="item form-group">
+							<textarea class="form-control blogTextarea" type="text" name="productnote"  id="blog_desc"> '.$row['blog_desc'].' </textarea>
+						</div>';
+							}
 								}
 								?>
 
-                       <div class="row">
-                       	<div class="col-md-4">
-                       		<div class="item form-group" style="text-align: right">
-                       		<button class="btn btn-danger blogButtons" type="button"><a style="color:white" href="view_blogs.php">Cancel</a></button>
-                       		</div>
-                       	</div>
-                       	<div class="col-md-4">
-                       		<div class="item form-group" style="text-align: center;">
-                       		<button class="btn btn-primary blogButtons" type="reset">Reset</button>
-                       	</div>
-                       	</div>
-                       	<div class="col-md-4" >
-                       		<div class="item form-group" style="text-align: left;">
-                            <button type="button" id="blogsubmit" class="btn btn-success blogButtons">Update</button>
-                        </div>
-                       	</div>
-                       </div>
 
 					</div>
 
 
+					<div class="col-md-3"></div>
+				</div>
 
-		</div>
-
-	</div>
-
-	<!-- <div class="col-md-6">
-		<div class="row">
-			<div class="col-md-12">
-				
-				<h4 style="color: white;text-align: center;" class="mb-4">CHOOSE A BLOG PICTURE!</h4>
-				<div class="form-group" style="text-align: center;">
-
-						<button type="button" class="btn btn-primary"><input type="file" id="blog_pic" name="file" /></button>	
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="item form-group" style="text-align: right">
+									<button class="btn btn-danger blogButtons" type="button"><a style="color:white" href="view_blogs.php">Cancel</a></button>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="item form-group" style="text-align: center;">
+									<button class="btn btn-primary blogButtons" type="reset">Reset</button>
+								</div>
+							</div>
+							<div class="col-md-4" >
+								<div class="item form-group" style="text-align: left;">
+									<button type="button" id="blogsubmit" class="btn btn-success blogButtons">Update</button>
+								</div>
+							</div>
 						</div>
-						<div class="form-group" style="text-align: center;">
-						<img src="images/default.png" id="img" width="60%" height="40%">
-						<div><input type="hidden" name="image1" id="image1" disabled></div>
-                        </div>
 					</div>
+					<div class="col-md-3"></div>
+				</div>
 
-			</div>
- 
+
+
+			</form>
 		</div>
-
-
-	</div> -->
-</form>
-</div>	
+	</section>
 
 
 
 
-		<?php
-		require '../footer.php';
-		?>
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
-
-		
 
 
-		<script type="text/javascript">
-			
 
-			$("#blogsubmit").click(function(){
 
-  
- 			var blog_id=$("#blog_id").val();
-  			var blog_title=$("#blog_title").val();
- 			var blog_desc=$("#blog_desc").val();
-  
 
- 			var datastr='blog_id='+blog_id+'&blog_title='+blog_title+'&blog_desc='+blog_desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<?php
+	require '../footer.php';
+	?>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<script src="../js/bootstrap.min.js"></script>
+
+
+	<script type="text/javascript">
+
+
+		$("#blogsubmit").click(function(){
+
+
+			var blog_id=$("#blog_id").val();
+			var blog_title=$("#blog_title").val();
+			var blog_desc=$("#blog_desc").val();
+
+
+			var datastr='blog_id='+blog_id+'&blog_title='+blog_title+'&blog_desc='+blog_desc;
 
 
 			if (blog_title=='') {
-			   alert("Please fill blog title")
-			 }
-			 else if (blog_desc=='') {
-			   alert("Please fill blog description");
-			 }
- 
+				alert("Please fill blog title")
+			}
+			else if (blog_desc=='') {
+				alert("Please fill blog description");
+			}
+
 
 			else{
 
-			 $.ajax({
+				$.ajax({
 
 
-			  type: "POST",
-			  url: "update_blog1.php",
-			  data: datastr,
-			  cache: false,
-			  success: function(res){
-			    if(res==1)
-			    {
-			      alert("success");
-			      window.location.href="view_blogs.php";
+					type: "POST",
+					url: "update_blog1.php",
+					data: datastr,
+					cache: false,
+					success: function(res){
+						if(res==1)
+						{
+							alert("Your blog has been updated!");
+							window.location.href="view_blogs.php";
 
-			    }
-			    else{
-
-
-			     alert("Something Went Wrong!");
+						}
+						else{
 
 
-			   }
+							alert("Something Went Wrong!");
 
-			 }
-			}); 
+
+						}
+
+					}
+				}); 
 			}
 
 
@@ -207,7 +192,9 @@
 
 
 
-});
-		</script>
-	</body>
-	</html>
+		});
+	</script>
+
+
+</body>
+</html>
--- view_blogs/view_blogs.php
+++ view_blogs/view_blogs.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Blogging</title>
+  <title>Blogging | YOUR BLOGS</title>
 
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 
   <link rel="stylesheet" type="text/css" href="../style.css">
 
   <link rel="stylesheet" type="text/css" href="../sidebar.css">
 
-  
+
+
+
 
 </head>
 <body>
@@ -26,65 +30,121 @@
 
 
 
-<div class="container">
- <div class="row">
-  <div class="col-md-1"></div>
-  <div class="col-md-11">
-
-<?php
-require '../connect.php';
-$sql= $conn->prepare("SELECT * FROM blogs where user_id='".$_SESSION['user_id']."' and status='0' order by created_date_time DESC");
-            $sql->execute();
-            $sql->setFetchMode(PDO::FETCH_ASSOC);
-            if($sql->rowCount()>0){
-              foreach (($sql->fetchAll()) as $key => $row) {
-
-                echo '<div class="jumbotron p-0 roundShadow" id="jumbotron">
-
-  
-  <div class="view overlay rounded-top">
-  
-    <img src="../'.$row['blog_pic'].'" id="blog_image" class="img-fluid" alt="Blog image">
-    <a href="#">
-      <div class="mask rgba-white-slight"></div>
-    </a>
-  </div>
-
-  
-  <div class="card-body text-left mb-3">
-    <small>Likes   |  </small>
-    <input type="button" value="Edit" onclick=\'editfunc("' .$row["blog_id"]. '")\' >
-    <input type="button" value="Delete" onclick=\'deletefunc("' .$row["blog_id"]. '")\' >
-   
-    <h3 class="card-title h3 my-4"><strong>'.$row['blog_title'].'</strong></h3>
-    <p style="font-size:18px;text-align:left;">'.$row['blog_desc'].'</p>
-    ';
-
-   
-
-              }
-          }
-?>
+  <section>
+    <div class="container">
 
 
-</div>
- </div> 
 
-</div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10 text-center">
+          <h1 style="color: white">YOUR BLOGS</h1>
+        </div>
+      </div>
+
+
+      <div class="row" style="margin-top: 50px;">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">
+
+       
+          <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
 
 
 
 
+            <thead>
 
-  <?php
-  require '../footer.php';
-  ?>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  
-  <script src="../js/bootstrap.min.js"></script>
 
-<script type="text/javascript">
+              <tr bgcolor="#29AAE2" class="text-center">
+
+                <th><font color="white">Blod title</font></th>
+                <th><font color="white">Blog Description</font></th>
+                <th><font color="white">Blog Image</font></th>
+                <th><font color="white">Action</th>
+
+                </tr>
+              </thead>
+              <tbody>
+
+
+
+                <?php
+                require '../connect.php';
+                $sql= $conn->prepare("SELECT * FROM blogs where user_id='".$_SESSION['user_id']."' and status='0' order by created_date_time DESC");
+                $sql->execute();
+                $sql->setFetchMode(PDO::FETCH_ASSOC);
+                if($sql->rowCount()>0){
+                  foreach (($sql->fetchAll()) as $key => $row) {
+
+                    echo '<tr>            
+
+
+                    <td  bgcolor="#2f3e46"><h6 style="color:white;">'.$row['blog_title'].'</h6></td>
+                    <td  bgcolor="#2f3e46"><p style="color:white;">'.$row['blog_short_desc'].'</p></td>
+                    <td  bgcolor="#2f3e46"><img src="../'.$row['blog_pic'].'" id="view_blog_img"  alt="Blog image"></td>
+                    <td  bgcolor="#2f3e46">
+
+                    <div class="btn-group">
+                    <input type="button" class="btn btn-primary" value="Edit" onclick=\'editfunc("' .$row["blog_id"]. '")\' >
+                    <input type="button" class="btn btn-danger" value="Delete" onclick=\'deletefunc("' .$row["blog_id"]. '")\' >
+                    </div>
+
+                    </td>
+
+
+
+
+
+                    </tr>';
+                  }
+                }
+                ?>
+              </tbody>
+            </table>
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    require '../footer.php';
+    ?>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="../js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
   
     function deletefunc(id)
     { 
@@ -127,8 +187,5 @@ function editfunc(id)
 
 
 
-
-
-
-</body>
-</html>
+  </body>
+  </html>
