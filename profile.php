101f7859bec1af0964093a750eb86db9bd476024
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-09 09:38:44 +0530

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Blogging | PROFILE</title>
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+  <script src="https://kit.fontawesome.com/05315665b2.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <link rel="stylesheet" type="text/css" href="sidebar.css">
+
+
+
+
+
+</head>
+<body>
+
+  <?php
+  require 'header.php';
+  ?>
+
+
+
+  
+
+
+<section>
+<div class="container" style="margin-top: 100px;background-color: white;padding: 50px;" id="container_shadow">
+
+
+<div class="row">
+
+  <div class="col-md-3 text-center">
+    <img src="profile_pictures/lloyd.jpeg" id="profile_page_pic" width="100" height="100">
+     
+  </div>
+
+  <div class="col-md-3 text-center">
+    
+    <h4 style="margin-top: 30px; "><strong>66</strong></h4>
+    <a href="" style="color: black;font-size: 20px;"><p>Posts</p></a>
+    
+  </div>
+  <div class="col-md-3 text-center">
+    <h4 style="margin-top: 30px; "><strong>1,462</strong></h4>
+    <a href="" style="color: black;font-size: 20px;"><p>Followers</p></a>
+  </div>
+  <div class="col-md-3 text-center">
+    <h4 style="margin-top: 30px; "><strong>1,734</strong></h4>
+    <a href="" style="color: black;font-size: 20px;"><p>Following</p></a>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <h4 style="margin-top: 20px;margin-left: 60px; text-align: left; ">Lloyd Dias</h4>
+    <p style="margin-left: 70px;margin-top: -6px; text-align: left;font-size: 18px ">Body builder</p>
+    <p style="margin-left: 70px;margin-top: -15px; text-align: left;font-size: 18px ">23</p>
+    <p style="margin-left: 70px;margin-top: -15px; text-align: left;font-size: 18px ">Goa University | Taleigao</p>
+    <p style="margin-left: 70px;margin-top: -15px; text-align: left;font-size: 18px ">Insert your quote here</p>
+    <p style="margin-left: 70px;margin-top: -15px; text-align: left;font-size: 18px ">Snapchat : LloydDias.example</p>
+    <a href=""><p style="margin-left: 70px;margin-top: -15px; text-align: left;font-size: 18px ">www.lloydDias.com</p></a>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12 text-center">
+
+    <button type="button" class="btn btn-primary form-control" style="width:900px;border:1px solid grey;"><a href="" style="color: white">Follow</a></button>
+
+    <hr/>    <!-----  Horizontal rule   ----->
+  </div>
+</div>
+
+
+
+
+<div class="row" style="margin-left: 50px;margin-right: 50px">
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/lloyd.jpeg" id="profile_blogs_image">
+    
+  </div>
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/sai.jpeg" id="profile_blogs_image">
+    
+  </div>
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/viru.jpeg" id="profile_blogs_image">
+    
+  </div>
+</div>
+
+<div class="row" style="margin-left: 50px;margin-right: 50px">
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/lloyd.jpeg" id="profile_blogs_image">
+    
+  </div>
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/lloyd.jpeg" id="profile_blogs_image">
+    
+  </div>
+  <div class="col-md-4 text-center" style="padding: 0px;">
+
+    <img src="profile_pictures/lloyd.jpeg" id="profile_blogs_image">
+    
+  </div>
+</div>
+
+
+
+
+
+</div>
+</section>
+
+
+
+
+
+
+  <?php
+require 'footer.php';
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
