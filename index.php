c6a905fc4089e683b45231ff8d17dfe5d7a9e8b3
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-11 21:25:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Blogging</title>
+  <title>BLOGG IT!</title>
 
 
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
-  <script src="https://kit.fontawesome.com/05315665b2.js" crossorigin="anonymous"></script>
-
   <link rel="stylesheet" type="text/css" href="style.css">
 
   <link rel="stylesheet" type="text/css" href="sidebar.css">
+
+ <link href="css/all.css" rel="stylesheet"> 
+
+ <script defer src="js/all.js"></script>
 
 
 
@@ -29,115 +31,10 @@
   ?>
 
 
-
-
-  <section>              <!----------------COUNTER SECTION ------------------------>
-
-    <div class="row" style="margin-left: 160px;margin-top: -20px;">
-      <div class="col-md-2"></div>
-      <div class="col-md-8 text-center">
-        
-
-        <div class="container-fluid" style="padding: 1px;margin-bottom: 20px;background-color: #ffffff; width: 900px;" id="index_card">
-
-
-          <div class="row" style="margin-top: 5px">
-            <div class="col-md-4 text-center">
-              <?php
-              $count=$conn->prepare("SELECT * FROM blogs where user_id='".$_SESSION['user_id']."' and status='0'");
-
-        $count->execute();
-
-        $no_of_blogs=$count->rowCount(); 
-        
-        if($no_of_blogs == ''){
-          $no_of_blogs=0;
-        }
-
-        echo '
-    <h4 style="margin-top: 30px; "><strong>'.$no_of_blogs.'</strong></h4>';
-             ?>
-             <p style="font-size:20px;">Blogs</p>
-           </div>
-           <div class="col-md-4 text-center">
-            
-            <?php
-              $count=$conn->prepare("SELECT * FROM following where following_id='".$_SESSION['user_id']."' and status='1'");
-
-        $count->execute();
-
-        $no_of_followers=$count->rowCount(); 
-        
-        if($no_of_followers == ''){
-          $no_of_followers=0;
-        }
-
-        echo '
-    <h4 style="margin-top: 30px; "><strong>'.$no_of_followers.'</strong></h4>';
-             ?>
-             
-             <p style="font-size:20px;">Followers</p>
-           </div>
-
-           <div class="col-md-4 text-center">
-            
-            <?php  
-            $count=$conn->prepare("SELECT * FROM following where user_id='".$_SESSION['user_id']."' and status='1'");
-
-        $count->execute();
-
-        $no_of_following=$count->rowCount(); 
-        
-        if($no_of_following == ''){
-          $no_of_following=0;
-        }
-
-        echo '<h4 style="margin-top: 30px; "><strong>'.$no_of_following.'</strong></h4>';
-            
-           
-           ?>
-           <p style="font-size:20px;">Following</p>
-         </div>
-       </div>
-
-       
-
-     </div>
-
-
-
-
-   </div>
-   <div class="col-md-2"></div>
- </div>
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</section>
-
-
-
-
-
-
-
 <section>
   
   <div class="container">
-   <div class="row">
+   <div class="row" >
     <div class="col-md-2"></div>
     <div class="col-md-10">
 
@@ -150,13 +47,16 @@
         foreach (($sql->fetchAll()) as $key => $row) {
 
 
-          $blog_title=$row['blog_title'];
-          $blog_desc=$row['blog_desc'];
-
-          echo '<div class="jumbotron p-0 roundShadow" id="jumbotron" style="background-color:#ffffff;">
 
 
-          <div class="view overlay rounded-top">
+        
+
+
+        if (isset($_SESSION['user_name'])){
+        echo '<div class="jumbotron p-0 "  style="background-color:#ffffff;margin-top:0px;border:2px solid #f3f3f3;border-radius:10px;" >
+
+
+          <div class="view overlay rounded-top" style="padding:20px;">
           <img src="'.$row['blog_pic'].'" id="blog_image" class="img-fluid" alt="Blog image">
           <a href="#">
           <div class="mask rgba-white-slight"></div>
@@ -164,44 +64,40 @@
           </div>
 
 
-          <div class="card-body text-left mb-3">
+          <div class="card-body text-left mb-3" style="margin-top:-50px">
 
 
-          <h3 class="card-title h3 my-4"><strong>'.$row['blog_title'].'</strong></h3>
+          <h3 class="card-title h3 my-4" style="color:#1b2668"><strong>'.$row['blog_title'].'</strong></h3>
 
 
-          <div class="row">
+          <div class="row" style="margin-top:-22px;margin-bottom:-10px;">
           <div class="col-md-8">
-          <p style=margin-bottom:5px;><i>Posted by: <a href="profile/profile.php?profile_id='.$row['user_id'].'">'.$row['username'].'</a></i></p>
-          <p style="font-size:11px;margin-top:-10px;"><i>created_date_time: '.$row['created_date_time'].'</i></p>
+          <p>Posted by: <a href="profile/profile.php?profile_id='.$row['user_id'].'" style="margin-right:10px;">'.$row['username'].'</a> '.$row['created_date_time'].'</p>
+         
           </div>';
 
           require 'likes/likes.php'; 
           echo '
           </div>
+          <hr/>
 
+         
 
-
-          <p style="font-size:16px;text-align:left;">'.$row['blog_short_desc'].'</p>
+          <p style="font-size:16px;text-align:left;">'.$row['blog_short_desc'].'....<a data-toggle="modal" data-target="#'.$row['blog_id'].'"  href="">Read More</a></p>
+          
 
           <div class="row">
-          <div class="col-md-9 text-center">
-          <div class="row">
+          <div class="col-md-12 text-center">
+          <div class="row" style="margin-right:6px;margin-bottom:-14px;">
           <div class="col-md-12" id="commentDiv">
-          <input style="margin-left:15px;background-color:#f8f9fb;" class="form-control commentfix" type="" id="comment_text'.$row["blog_id"].'"  placeholder="Leave a comment...."> 
-          <button style="width:80px;margin-left:-80px;" type="button" class="btn btn-light" onclick=\'commentfunc("' .$row["blog_id"]. '")\' ><i class="fas fa-paper-plane" style="font-size:20px;"></i></button>
+          <input style="margin-left:15px;background-color:#cfe0ee;" class="form-control commentfix" type="" id="comment_text'.$row["blog_id"].'"  placeholder="Leave a comment...."> 
+          <button style="width:80px;margin-left:-80px;background-color:#cfe0ee;" type="button" class="btn" onclick=\'commentfunc("' .$row["blog_id"]. '")\' ><i class="fas fa-paper-plane" style="font-size:20px;"></i></button>
           </div>
 
 
           </div> 
           </div>
-          <div class="col-md-3 text-right">
-          <div class="btn-group">
-
-          <button style="width:80px;margin-top:2px;" type="button" class="btn btn-primary" data-toggle="modal" data-target="#'.$row['blog_id'].'"><i class="fas fa-book-open" style="font-size:18px;"></i></button>
-          <button style="width:80px;margin-top:2px;" type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal'.$row['blog_id'].'"><i class="fas fa-exclamation-triangle" style="font-size:18px;"></i></button>
-          </div>
-          </div>
+          
           </div>
 
           <!-- Modal -->
@@ -246,17 +142,17 @@
           <div class="modal fade right" id="'.$row['blog_id'].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalPreviewLabel" aria-hidden="true">
           <div class="modal-dialog-full-width modal-dialog momodel modal-fluid" role="document">
           <div class="modal-content-full-width modal-content ">
-          <div class=" modal-header-full-width   modal-header text-center">
-          <h5 class="modal-title w-100" id="exampleModalPreviewLabel">BLOGGING WEBSITE</h5>
-          <button type="button" class="close " data-dismiss="modal" aria-label="Close">
+          <div class=" modal-header-full-width   modal-header text-center" style="background-image: linear-gradient(to left, #367cff, #26c0f7);">
+          <h3 class="modal-title w-100" style="color:white;"><strong>BLOGG IT!</strong></h3>
+          <button style="color:white;" type="button" class="close " data-dismiss="modal" aria-label="Close">
           <span style="font-size: 1.3em;" aria-hidden="true">&times;</span>
           </button>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" style="background-color:#ffffff">
 
           <img id="read_more_img" src="'.$row['blog_pic'].'">
 
-          <h1 class="section-heading text-center wow fadeIn my-5 pt-3">'.$row['blog_title'].'</h1>
+          <h1 style="color:#382d51; margin-top:20px;" class="section-heading text-center wow fadeIn ">'.$row['blog_title'].'</h1>
 
           <p class="text-center" style="margin-left:5%;margin-right:5%">'.$row['blog_desc'].'</p>
 
@@ -299,6 +195,167 @@
           </div>
           </div>';
 
+          
+        }
+
+        else{
+
+          echo '<div class="jumbotron p-0 "  style="background-color:#ffffff;margin-top:0px;border:2px solid #f3f3f3;border-radius:10px;" >
+
+
+          <div class="view overlay rounded-top" style="padding:20px;">
+          <img src="'.$row['blog_pic'].'" id="blog_image" class="img-fluid" alt="Blog image">
+          <a href="#">
+          <div class="mask rgba-white-slight"></div>
+          </a>
+          </div>
+
+
+          <div class="card-body text-left mb-3" style="margin-top:-50px">
+
+
+          <h3 class="card-title h3 my-4" style="color:#1b2668"><strong>'.$row['blog_title'].'</strong></h3>
+
+
+          <div class="row" style="margin-top:-22px;margin-bottom:-10px;">
+          <div class="col-md-8">
+          <p>Posted by: <a href="profile/profile.php?profile_id='.$row['user_id'].'" style="margin-right:10px;">'.$row['username'].'</a> '.$row['created_date_time'].'</p>
+         
+          </div>
+
+
+          </div>
+          <hr/>
+
+         
+
+          <p style="font-size:16px;text-align:left;">'.$row['blog_short_desc'].'....<a data-toggle="modal" data-target="#'.$row['blog_id'].'"  href="">Read More</a></p>
+          
+
+          <div class="row">
+          <div class="col-md-12 text-center">
+          <div class="row" style="margin-right:6px;margin-bottom:-14px;">
+          <div class="col-md-12" id="commentDiv">
+         
+          </div>
+
+
+          </div> 
+          </div>
+          
+          </div>
+
+          <!-- Modal -->
+          <div class="text-center">
+          <div id="myModal'.$row['blog_id'].'" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+          <div class="modal-header">
+          <h4>Report this Blog</h4>
+          </div>
+          <div class="modal-body">
+          <p>We appretiate your report feedback!</p> <br>
+
+
+          <select name="category" id="report_category' .$row['blog_id']. '" class="form-control" required> 
+          <option value="" > Select Category</option>
+          <option value="spam" > Spam </option>
+          <option value="pornography" > Pornography </option>
+          <option value="Hatred and Bullying" > Hatred and Bullying </option>
+          <option value="Self-harm" > Self-harm </option>
+          <option value="Violant contnet" > Violant,gory and harmful content </option>
+          <option value="Illegal activities" > Illegal activities(e.g drug use)</option>
+          <option value="Copyright infrigement" > Copyright and trademark infrigement</option>
+          <option value="Dont like it" > I just don\'t like it</option>
+          </select>
+
+
+          <input style="margin-top: 1%;" class="form-control" type="text" name="report_desc" id="report_desc' .$row['blog_id']. '" placeholder="Write report description..." required>
+          </div>
+          <div class="modal-footer">
+
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" onclick=\'reportfunc("' .$row['blog_id']. '")\' class="btn btn-danger" data-dismiss="modal">Report</button>
+          </div>
+          </div>
+
+          </div>
+          </div>
+          </div>
+          <div class="modal fade right" id="'.$row['blog_id'].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalPreviewLabel" aria-hidden="true">
+          <div class="modal-dialog-full-width modal-dialog momodel modal-fluid" role="document">
+          <div class="modal-content-full-width modal-content ">
+          <div class=" modal-header-full-width   modal-header text-center" style="background-image: linear-gradient(to left, #367cff, #26c0f7);">
+          <h3 class="modal-title w-100" style="color:white;"><strong>BLOGG IT!</strong></h3>
+          <button style="color:white;" type="button" class="close " data-dismiss="modal" aria-label="Close">
+          <span style="font-size: 1.3em;" aria-hidden="true">&times;</span>
+          </button>
+          </div>
+          <div class="modal-body" style="background-color:#ffffff">
+
+          <img id="read_more_img" src="'.$row['blog_pic'].'">
+
+          <h1 style="color:#382d51; margin-top:20px;" class="section-heading text-center wow fadeIn ">'.$row['blog_title'].'</h1>
+
+          <p class="text-center" style="margin-left:5%;margin-right:5%">'.$row['blog_desc'].'</p>
+
+
+          <div class="row">
+          <div class="col-md-12">
+          <h4 style="margin-left:20px;">Comments</h4>
+          <hr/>
+          </div>
+          </div>';
+
+
+          require 'comments/comment.php';
+
+          echo '
+
+          </div>
+          <div class="modal-footer-full-width  modal-footer">
+          <button type="button" class="btn btn-danger btn-md btn-rounded" data-dismiss="modal">Close</button>
+          <!-- <button type="button" class="btn btn-primary btn-md btn-rounded">Save changes</button> -->
+          </div>
+          </div>
+          </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
+          </div>';
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
         }
       }
       ?>
