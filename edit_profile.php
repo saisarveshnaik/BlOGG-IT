30350894a014a4e700a218df2c2c77c030b3a6bd
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-11 21:58:18 +0530

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -1,256 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Blogging | Edit Profile</title>
-
-
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-	<script src="https://kit.fontawesome.com/05315665b2.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="style.css">
-
-	<link rel="stylesheet" type="text/css" href="sidebar.css">
-
-
-</head>
-<body>
-
-
-
-
-  <?php
-  require 'header.php';
-  ?>
-
-
-
-	<section>
-		<div class="container-fluid" style="background-color: #fafafa"> 
-
-
-
-
-			<div class="row">
-
-				<div class="col-md-3"></div>
-				<div class="col-md-6">
-					<div class="container-fluid" style="padding: 1px;background-color: #ffffff;margin-top: 50px;" id="about_card">
-
-						<div class="row" style="margin-top: 40px;">
-							<div class="col-md-12 text-center">
-								<h4 style="color: #4c4c4c">Edit Profile</h4>
-								<hr/>
-							</div>
-						</div>
-
-
-
-						<!-- <div class="row">
-						 <div class="col-md-12 text-center">
-						  <img src="images/profile.png" id="img" width="100" height="100"></img>
-						  <input type="hidden" name="image1" id="image1" disabled>
-						  <button style="width:200px; height:30px;border:none;margin-top: 10px;background-color: #ffffff;  color: #027ddb;" onclick="document.getElementById('profile_pic').click()">Change profile photo</button>
-                          <input type='file' id="profile_pic" name="file" style="display:none">
-						 </div>	
-						</div> -->
-						<div class="row" style="">
-						<div class="col-md-12 text-center">
-							<img src="images/profile.png" id="img" width="100" height="100"></img>
-							<input type="hidden" name="image1" id="image1" disabled>
-							<button style="width:200px; height:30px;border:none;margin-top: 10px;background-color: #ffffff;  color: #027ddb;" onclick="document.getElementById('profile_pic').click()">Choose profile photo</button>
-							<input type='file' id="profile_pic" name="file" style="display:none">
-						</div>	
-					</div>
-
-
-
-						<div class="row" style="">							 
-						<div class="col-md-2"></div>
-						<div class="col-md-8" style="padding: 20px;">
-						
-						<p style="color: #4c4c4c">Name</p>
-						<input type="text" id="name" name="" class="form-control input_border" style="margin-top: -18px;">
-
-						<p style="margin-top: 20px;color: #4c4c4c">Username</p>
-						<input type="text" id="username" name="" class="form-control input_border" style="margin-top: -18px;">
-
-						<p style="margin-top: 20px;color: #4c4c4c">Address</p>
-						<input type="text" id="address" name="" class="form-control input_border" style="margin-top: -18px;">	
-
-						<p style="margin-top: 20px;color: #4c4c4c">Email</p>
-						<input type="text" id="email" name="" class="form-control input_border" style="margin-top: -18px;">
-
-						<p style="margin-top: 20px;color: #4c4c4c">Password</p>
-						<input type="text" id="password" name="" class="form-control input_border" style="margin-top: -18px;">
-
-						<div class="row" style="margin-top: 20px;margin-bottom:40px; ">
-							<div class="col-md-12 text-center">
-							<button type="button" style="width: 200px;background-color: #e2e2e2;" id="save_changes" class="btn btn-light" align='center'>Save Changes</button>	
-							</div>
-						</div>
-
-						
-
-						</div>	
-
-						<div class="col-md-2"></div> 
-
-
-						</div>
-						
-                     
-
-
-					</div>
-				<div class="col-md-3"></div>
-
-			</div>
-
-
-		</div>
-	</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
-  require 'footer.php';
-  ?>
+require 'connect.php';
+require 'session_script.php';
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-  <script src="js/bootstrap.min.js"></script>
-
-  <script>
-		$('#profile_pic').change(function(){
-
-			var fd = new FormData();
-			var files = $('#profile_pic')[0].files[0];
-			fd.append('file',files);
-
-			$.ajax({
-				url: 'upload.php',
-				type: 'post',
-				data: fd,
-				contentType: false,
-				processData: false,
-				success: function(response){
-					if(response != 0){
-						$("#img").attr("src",response); 
-						$('#image1').val(response);
-                    $(".preview img").show(); // Display image element
-                }else{
-                	alert('file not uploaded');
-                }
-            },
-        });
-		});
-
-	</script>
-		
-		
-		
-		
-		
-		
-		<script>
-			$("#save_changes").click(function(){
-
-		
-		var name=$("#name").val();
-		var username=$("#username").val();
-		var email=$("#email").val();
-		var address=$("#address").val();
-		var password=$("#password").val();
-		var profile_pic=$("#image1").val();
-			
-
-
-
-		var datastr='password='+password+'&name='+name+'&username='+username+'&email='+email+'&address='+address+'&profile_pic='+profile_pic;
+$name=$_POST['name'];
+$email=$_POST['email'];
+$username=$_POST['username'];
+$address=$_POST['address'];
+$password=$_POST['password'];
+$created_by=$_SESSION['user_id'];
+$profile_pic=$_POST['profile_pic'];
 
 
 
 
+$sql="UPDATE users SET user_name = '".$name."', username = '".$username."', user_email = '".$email."', user_address = '".$address."', password = '".$password."', profile_pic = '".$profile_pic."' where user_id= '".$_SESSION["user_id"]."' ";
+$stmt= $conn->prepare($sql);
+$result= $stmt->execute(array(
+	':name'=>$name,
+	':email'=>$email,
+	':username'=>$username,
+	':address'=>$address,
+	':password'=>$password,
+	':profile_pic'=>$profile_pic,
+	':created_by' =>$created_by
+));
 
 
-		if (password=='') {
-		alert("Please Enter the Password");
-
-		}
-		else if(name==''){
-		alert("Please Enter the username");
-
-		}
-
-		else if (username=='') {
-		alert("Please fill blog title")
-		}
-
-		else if (email=='') {
-		alert("Please provide a short blog description")
-		}
-
-
-		else if (address=='') {
-		alert("Please enter the address");
-
-
-
-		}
-
-
-		else{
-
-		$.ajax({
-
-
-		type: "POST",
-		url: "edit_profile_GET.php",
-		data: datastr,
-		cache: false,
-		success: function(res){
-		if(res==1)
-		{
-			window.location.href="logout.php";
-			alert("Profile Edited Successfully");
-			
-			window.location.href="login.php";
-
-
-		}
-		else{
-
-
-		alert("Error");
-
-
-		}
-
-		}
-		}); 
-		}
-
-		});
+if($result)
+{
+	echo "1";
+}
+else
+{
+	echo "0";
+}
 
 
 
-
-
-		
-	
-	</script>
-
-</body>
-</html>
+?>
