baf3ead93ed188ec22dc5d06ae9721f794ef0a91
saisarveshnaik <43439606+saisarveshnaik@users.noreply.github.com>
2020-08-11 21:00:26 +0530

    Update about_us.php

--- profile/about_us.php
+++ profile/about_us.php
@@ -1,188 +1,85 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Blogging | ABOUT US</title>
-
-
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-  <script src="https://kit.fontawesome.com/05315665b2.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" type="text/css" href="style.css">
-
-  <link rel="stylesheet" type="text/css" href="sidebar.css">
-
-
-
-
-
-</head>
-<body>
-
-  <?php
-  require 'header.php';
-  ?>
-
-
-
-
-
-
-
-
-  <section>
-    <div class="container" style="margin-top: 50px">
-
-   <div class="row">
-    <div class="col-md-12 text-center">
-      <h1 style="color: white">ABOUT US</h1>
-    </div>
-  </div> 
-  </div>
-</section>
-<!----------------------------------------------------------------------------------------------------->
-<section>
-  <div class="container">
-  <div class="row" style="margin-top: 120px;">
-    <div class="col-md-3">
-  <div class="card" style="width: 15rem;" id="about_card">
-    <img class="card-img-top" style="margin-top: 5px;"  src="profile_pictures/viru.jpeg" alt="Profile image" id="about_us_img">
-    <div class="card-body">
-      <h5 class="card-title">Virendra Phoolwari</h5>
-      <p class="card-text"><i>Web developer</i></p>
-      <p style="margin-top: -20px">Quality assurance</p>
-
-
-      
-<div class="row">
-  <div class="button-group">
-  <div class="col-md-12" style="margin-left: 24px">
-    <button  onclick="window.location.href='';" type="button" name="github" class="btn btn-success" style="text-align:left"><i class="fab fa-github" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="linkedin" class="btn btn-primary" style="text-align:center;"><i class="fab fa-linkedin-in" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="youtube" class="btn btn-danger" style="text-align:right"><i class="fab fa-youtube" style="font-size: 20px;"></i></button>
-  </div>
-  </div>
-</div>
-
-
-
-    </div>
-  </div>
-</div>
-    <div class="col-md-3">
-  <div class="card" style="width: 15rem;" id="about_card">
-    <img class="card-img-top" style="margin-top: 5px;" src="profile_pictures/sai.jpeg" alt="Profile image" id="about_us_img">
-    <div class="card-body">
-      <h5 class="card-title">Saisarvesh Naik</h5>
-      <p class="card-text"><i>Full stack developer</i></p>
-      <p style="margin-top: -20px">UI designer</p>
-
-
-     <div class="row">
-  <div class="button-group">
-  <div class="col-md-12" style="margin-left: 24px">
-    <button onclick="window.location.href='';" type="button" name="github" class="btn btn-success" style="text-align:left"><i class="fab fa-github" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="linkedin" class="btn btn-primary" style="text-align:center;"><i class="fab fa-linkedin-in" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="youtube" class="btn btn-danger" style="text-align:right"><i class="fab fa-youtube" style="font-size: 20px;"></i></button>
-  </div>
-  </div>
-</div>
-
-
-    </div>
-  </div>
-</div>
-    <div class="col-md-3">
-  <div class="card" style="width: 15rem;" id="about_card">
-    <img class="card-img-top" style="margin-top: 5px;" src="profile_pictures/lloyd.jpeg" alt="Profile image" id="about_us_img">
-    <div class="card-body">
-      <h5 class="card-title">Lloyd Dias</h5>
-      <p class="card-text"><i>Back end developer</i></p>
-      <p style="margin-top: -20px">Database developer</p>
-
-
-     <div class="row">
-  <div class="button-group">
-  <div class="col-md-12" style="margin-left: 24px">
-    <button onclick="window.location.href='';" type="button" name="github" class="btn btn-success" style="text-align:left"><i class="fab fa-github" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="linkedin" class="btn btn-primary" style="text-align:center;"><i class="fab fa-linkedin-in" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="youtube" class="btn btn-danger" style="text-align:right"><i class="fab fa-youtube" style="font-size: 20px;"></i></button>
-  </div>
-  </div>
-</div>
-
-
-    </div>
-  </div>
-</div>
-    <div class="col-md-3">
-  <div class="card" style="width: 15rem;" id="about_card">
-    <img class="card-img-top" style="margin-top: 5px;" src="profile_pictures/lloyd.jpeg" alt="Profile image" id="about_us_img">
-    <div class="card-body">
-      <h5 class="card-title">Shubhan Gupta</h5>
-      <p class="card-text"><i>Web developer</i></p>
-      <p style="margin-top: -20px">Content Writer</p>
-
-
-      <div class="row">
-  <div class="button-group">
-  <div class="col-md-12" style="margin-left: 24px">
-    <button onclick="window.location.href='';" type="button" name="github" class="btn btn-success" style="text-align:left"><i class="fab fa-github" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="linkedin" class="btn btn-primary" style="text-align:center;"><i class="fab fa-linkedin-in" style="font-size: 20px;"></i></button>
-    <button onclick="window.location.href='';" type="button" name="youtube" class="btn btn-danger" style="text-align:right"><i class="fab fa-youtube" style="font-size: 20px;"></i></button>
-  </div>
-  </div>
-</div>
-
-    </div>
-  </div>
-</div>
-    
-  </div>
-</div>
-</section>
-
-
-
-
-
-
-
-
-
-<!-- <div class="row">
-  <div class="button-group">
-  <div class="col-md-4">
-    <button type="button" class="btn btn-primary">github</button>
-  </div>
-  <div class="col-md-4">
-    <button type="button" class="btn btn-primary"></button>
-  </div>
-  <div class="col-md-4">
-    <button type="button" class="btn btn-primary"></button>
-  </div>
-  </div>
-</div>
-
-
- -->
-
-
-
-
-
-
-
-
-
 <?php
-require 'footer.php';
+require '../connect.php';
+require '../session_script.php';
+
+error_reporting(0);
+
+
+$profile_id=$_GET['profile_id'];
+
+$user_id=$_SESSION['user_id'];
+
+$status='';
+
+$sql= $conn->prepare("SELECT * FROM following where following_id='".$profile_id."' and user_id='".$user_id."'");
+$sql->execute();
+$sql->setFetchMode(PDO::FETCH_ASSOC);
+if($sql->rowCount()>0){
+	foreach (($sql->fetchAll()) as $key => $row) {
+		
+
+	$status=$row['status'];
+	}
+	
+	
+
+}
+
+
+if(($_GET['status'])){
+     header('Location:add_profile.php');
+     
+}
+$count=$conn->prepare("SELECT * FROM following where following_id='".$profile_id."' and user_id='".$user_id."'");
+
+$count->execute();
+
+
+$number_of_rows2=$count->rowCount();
+
+
+        if($number_of_rows2 == ""){
+
+        	
+
+
+		$sql="INSERT INTO following(following_id, user_id) VALUES (:profile_id,:created_by)";
+		$stmt= $conn->prepare($sql);
+		$result= $stmt->execute(array(
+			':profile_id'=>$profile_id,
+			':created_by'=>$user_id
+		));
+	}
+	else if($number_of_rows2 == "1" && $status == "1"){
+		
+		$sql="UPDATE following SET status='2' WHERE following_id=:profile_id and user_id=:user_id";
+		$stmt= $conn->prepare($sql);
+		$result= $stmt->execute(array(
+			':profile_id'=>$profile_id,
+			':user_id' =>$user_id
+		));}
+	else if($number_of_rows2 == "1" && $status == "2"){
+			
+			$sql="UPDATE following SET status='1' WHERE following_id=:profile_id and user_id=:user_id";
+		$stmt= $conn->prepare($sql);
+		$result= $stmt->execute(array(
+			':profile_id'=>$profile_id,
+			':user_id' =>$user_id
+		));
+
+	}
+	
+
+
+if($result)
+{
+	echo "1";
+}
+else
+{
+	echo "0";
+}
+
+
+
 ?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<script src="js/bootstrap.min.js"></script>
-
-
-</body>
-</html>
